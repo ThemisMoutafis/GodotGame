9bc178d1f8ed35373f93a7c5547667479361ac83
Themis Moutafis <moutafisthemis@hotmail.com>
2026-01-26 16:39:52 +0200

    feat: initial project structure with C# pipeline and gitignore

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class HelloWorld : Node2D
+{
+	// If this works we're ready to go!
+	public override void _Ready()
+	{
+		GD.Print(">>> C# Pipeline is Live! If you can read this we can get to work!. <<<");
+	}
+
+	
+	
+}
